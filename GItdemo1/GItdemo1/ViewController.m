746d4a2dc59e497883da79df4b3bca980520b399
taoxingyu <810266270@qq.com>
2016-02-26 14:18:31 +0800

    first commit project

---
+++ GItdemo1/GItdemo1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GItdemo1
+//
+//  Created by qianfeng1 on 16/2/26.
+//  Copyright © 2016年 陶星宇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
