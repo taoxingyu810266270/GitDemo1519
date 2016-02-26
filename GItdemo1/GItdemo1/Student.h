59f9259d964a9931c604da81db8bec78d6fba619
taoxingyu <810266270@qq.com>
2016-02-26 14:36:07 +0800

    添加Student类

---
+++ GItdemo1/GItdemo1/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  GItdemo1
+//
+//  Created by qianfeng1 on 16/2/26.
+//  Copyright © 2016年 陶星宇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ GItdemo1/GItdemo1/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  GItdemo1
+//
+//  Created by qianfeng1 on 16/2/26.
+//  Copyright © 2016年 陶星宇. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
